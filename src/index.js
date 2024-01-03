b201d23a76965cb9e51a36164926f947186f0c15
Gary Nguyen <giangnguyen221295@gmail.com>
2024-01-04 00:05:21 +0700

    initial

--- src/index.js
+++ src/index.js
@@ -1,10 +1,28 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+// Fake comments
+function emitComment(id) {
+  setInterval(() => {
+    // như scroll, resize thì window sẽ tự emit event,
+    // nhưng ở đây ta sẽ tự tạo và phát ra 1 event ở tầng window
+    // => bất cứ file nào trong ứng dụng thì đều listen được
+    window.dispatchEvent(
+      new CustomEvent(`lesson-${id}`, {
+        detail: `This is comment of lesson ${id}`,
+      })
+    );
+  }, 1000);
+}
+
+emitComment(1);
+emitComment(2);
+emitComment(3);
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
     <App />
